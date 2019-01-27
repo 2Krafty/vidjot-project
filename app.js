5a4185ecb6ae6ab9886b10aa769912069feb8b0a
krafty chris <45397921+2Krafty@users.noreply.github.com>
2019-01-26 22:51:58 -0500

    updating

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 
+const exphbs = require('express-handlebars');
+
 const app = express();
 
-// index route
-app.get();
 
+
+//handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// How middleware works
+/*app.use((req, res, next) => {
+  //console.log(Date.now());
+  req.name = 'Christopher Kraft';
+  next();
+});*/
+
+// index route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
+
+// about route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 const port = 5000;
 
 app.listen(port, () => {
-  console.log(`server started on port ${port}`)
+  console.log(`Server started on port ${port}`);
 })
