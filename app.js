a6966c545abe71ddb541acfbeb1063f9a9e49634
krafty chris <45397921+2Krafty@users.noreply.github.com>
2019-02-05 10:27:32 -0500

    saving work

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-let express = require('express');
+const express = require('express');
 
-let exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
 
 let app = express();
+// map global
+mongoose.Promise = global.Promise;
+//connect to mongoose
+mongoose.connect('mongodb+srv://krafty:TalenJames2011@vidjot-dev-ndnxz.mongodb.net/test?retryWrites=true')
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-
+// load idea model
+require('./models/Idea');
+constIdea = mongoose.model('ideas');
 
 //handlebars middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // How middleware works
 /*app.use((req, res, next) => {
@@ -21,7 +37,8 @@ app.set('view engine', 'handlebars');
 
 // index route
 app.get('/', (req, res) => {
-  let title = 'Welcome1';
+
+  const title = 'Welcome';
   res.render('index', {
     title: title
   });
@@ -31,6 +48,16 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about');
 });
+// add idea form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+// process form
+app.post('/ideas', (req, res) => {
+
+  res.send('ok');
+})
+
 let port = 5000;
 
 app.listen(port, () => {
